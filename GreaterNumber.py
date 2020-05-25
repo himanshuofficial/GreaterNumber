8a09ee6274f0c811884ee3ed63d077d3552903a7
Alok Sharma <34425792+aloksharma999@users.noreply.github.com>
2020-05-25 16:46:49 -0400

    Add files via upload

---
+++ GreaterNumber.py
@@ -0,0 +1,8 @@
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number:'))
+if a > b:
+    print(a,'is the bigger number')
+elif a <b:
+    print(b,'is the bigger number')
+elif a ==b:
+    print(str(a) + 'is equal to ' +str(b))
