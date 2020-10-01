559ad801c35f2c4e86727c49d0d8d6430c0f8b26
Himanshu Sharma <62053260+himanshuofficial@users.noreply.github.com>
2020-10-01 16:24:35 +0530

    Update GreaterNumber.py
    shortcut to find max between two number

--- GreaterNumber.py
+++ GreaterNumber.py
@@ -1,8 +1,4 @@
 a = int(input('Enter the first number: '))
 b = int(input('Enter the second number:'))
-if a > b:
-    print(a,'is the bigger number')
-elif a <b:
-    print(b,'is the bigger number')
-elif a ==b:
-    print(str(a) + 'is equal to ' +str(b))
+
+print("Maximum number is:",max(a, b))
